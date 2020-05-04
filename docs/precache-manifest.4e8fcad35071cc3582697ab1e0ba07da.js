12400f53e5583fb5fd0b721f8730536f605965fb
Domestica <domesticanandina@gmail.com>
2020-05-04 14:41:16 +0900

    add docs

---
+++ docs/precache-manifest.4e8fcad35071cc3582697ab1e0ba07da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d9d2756366a6c91c83e4ff33248420",
+    "url": "/neumannproject/index.html"
+  },
+  {
+    "revision": "9b45d2f6d9a22b7fea08",
+    "url": "/neumannproject/static/css/main.e7cb2eed.chunk.css"
+  },
+  {
+    "revision": "4219d2e769013ef74be8",
+    "url": "/neumannproject/static/js/2.d2f4816c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neumannproject/static/js/2.d2f4816c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b45d2f6d9a22b7fea08",
+    "url": "/neumannproject/static/js/main.1b23f5b6.chunk.js"
+  },
+  {
+    "revision": "dde4e5b759af9b5aacdd",
+    "url": "/neumannproject/static/js/runtime-main.b8398ebe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neumannproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
